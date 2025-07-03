e3bca40d6172301f5f2fc3582431dceb109c60be
Yasir Hamid <ratherseenu16@gmail.com>
2025-07-03 12:44:16 +0530

    Your message

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const paymentRoute = require("./routes/payment");
 // ✅ CORS Configuration
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://silly-centaur-b9ac25.netlify.app",
+  "https://newageversatilestudio.netlify.app",
 ];
 
 app.use(
